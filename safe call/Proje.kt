fcf009fcb66d551d3c0d46829b3379f65a5d5cea
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 13:10:25 +0300

    Create Proje.kt

---
+++ safe call/Proje.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+   var str: String? = "kotlin öğreniyorum"
+   str = null
+
+      print("string uzunluğu = "+str?.length)
+
+}
