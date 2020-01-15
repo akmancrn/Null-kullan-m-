6ba82ae7c239af9b272fbe8a4796bf7e48f64267
Ceren Akman <42838160+akmancrn@users.noreply.github.com>
2020-01-15 12:55:35 +0300

    if kullanımı

---
+++ Proje.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+
+import java.io.PrintStream
+import java.util.Scanner
+import java.util.*
+
+fun main (args : Array<String>)
+{
+   var str:String?
+   str = null
+   if(str != null){
+      print("tring uzunluğu = "+str.length)
+   }
+   else
+      print("string null olduğu için if çalışmadı")
+}
